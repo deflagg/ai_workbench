cd5b95d91e82d89220de4b689fa9820fba6dca34
Dennis Flagg <deflagg@hotmail.com>
2025-02-21 00:06:35 -0500

    Reduce default number of attention heads to 4
    - Decreased default number of attention heads from 8 to 4 in wikitext2_enhanced.py experiment script

--- experiments/aaaa/wikitext2_enhanced.py
+++ experiments/aaaa/wikitext2_enhanced.py
@@ -339,7 +339,7 @@ def main():
                         help="W&B entity/username")
     parser.add_argument("--d_model", type=int, default=256,
                         help="Dimension of the token and positional embeddings.")
-    parser.add_argument("--nhead", type=int, default=8,
+    parser.add_argument("--nhead", type=int, default=4,
                         help="Number of attention heads in the decoder blocks.")
     parser.add_argument("--num_layers", type=int, default=2,
                         help="Number of decoder blocks.")
