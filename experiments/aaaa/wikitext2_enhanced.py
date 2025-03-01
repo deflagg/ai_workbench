4639fa95e2db0171b103eac9584db017d05b5320
Dennis Flagg <deflagg@hotmail.com>
2025-03-01 18:55:49 -0500

    Increase number of decoder layers from 2 to 3
    - Updated default num_layers from 2 to 3 in wikitext2_enhanced.py experiment script

--- experiments/aaaa/wikitext2_enhanced.py
+++ experiments/aaaa/wikitext2_enhanced.py
@@ -346,7 +346,7 @@ def main():
                         help="Dimension of the token and positional embeddings.")
     parser.add_argument("--nhead", type=int, default=2,
                         help="Number of attention heads in the decoder blocks.")
-    parser.add_argument("--num_layers", type=int, default=2,
+    parser.add_argument("--num_layers", type=int, default=3,
                         help="Number of decoder blocks.")
     parser.add_argument("--dim_feedforward", type=int, default=512,
                         help="Dimension of the feedforward network in the decoder blocks.")
